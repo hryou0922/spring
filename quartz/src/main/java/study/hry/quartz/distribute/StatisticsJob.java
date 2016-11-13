acae413a548c292d3e9f6d0a6fe96d8ffcba0ff8
hryou0922 <hryou0922@126.com>
2016-11-13 21:42:46 +0800

    quartz

--- quartz/src/main/java/study/hry/quartz/distribute/StatisticsJob.java
+++ quartz/src/main/java/study/hry/quartz/distribute/StatisticsJob.java
@@ -2,10 +2,12 @@ package study.hry.quartz.distribute;
 
 import java.util.Date;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+@DisallowConcurrentExecution
 public class StatisticsJob extends QuartzJobBean{
 
 	@Override
@@ -14,14 +16,14 @@ public class StatisticsJob extends QuartzJobBean{
 		System.out.println("测试定时任务----------------------------------------------------------------------------------");
 		Date start = new Date();
 		System.out.println("start:"+start);
-//		try {
-//			Thread.sleep(20000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		long end = new Date().getTime();
-		System.out.println("定时任务执行结束时间："+start);
+		try {
+			Thread.sleep(15000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = new Date().getTime();
+		System.out.println("定时任务执行结束时间："+start + ". end = " + new Date());
 	}
 
 }
